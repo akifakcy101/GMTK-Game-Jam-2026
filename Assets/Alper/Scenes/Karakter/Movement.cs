685692936443cb4c1ac0d9ad7ebfddc515589668
Alper Gülender <m.alpergulender@gmail.com>
2026-07-23 23:39:18 +0300

    Update Movement.cs

--- Assets/Alper/Scenes/Karakter/Movement.cs
+++ Assets/Alper/Scenes/Karakter/Movement.cs
@@ -5,14 +5,9 @@ public class Movement : MonoBehaviour
 {
     [Header("Speed Settings")]
     public float speed = 5f;
-    public float sprintSpeed = 8f;
-    public float slowSpeed = 2.5f;    
-    private float currentSpeed;     
     private Rigidbody2D rb;
     private Vector2 movement;
     private InputAction moveAction; 
-    private InputAction sprintAction;
-    private InputAction slowAction;
 
     void Awake()
     {
@@ -24,43 +19,25 @@ public class Movement : MonoBehaviour
             .With("Down", "<Keyboard>/s")
             .With("Left", "<Keyboard>/a")
             .With("Right", "<Keyboard>/d");
-            
-        sprintAction = new InputAction("Sprint", binding: "<Keyboard>/leftShift");
-        slowAction = new InputAction("Slow", binding: "<Keyboard>/leftCtrl");
+        
     }
     void OnEnable()
     {
         moveAction.Enable();
-        sprintAction.Enable();
-        slowAction.Enable();
     }
 
     void OnDisable()
     {
         moveAction.Disable();
-        sprintAction.Disable();
-        slowAction.Disable();
     }
 
     void Update()
     {
         movement = moveAction.ReadValue<Vector2>();    
-        if(sprintAction.IsPressed())
-        {
-            currentSpeed = sprintSpeed;
-        }
-        else if(slowAction.IsPressed())
-        {
-            currentSpeed = slowSpeed;
-        }
-        else
-        {
-            currentSpeed = speed;
-        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement.normalized * currentSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }
